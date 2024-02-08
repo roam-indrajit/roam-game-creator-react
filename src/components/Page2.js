f3c05d72e61f1a14f66365516e58c587192c5190
roam-indrajit <155762057+roam-indrajit@users.noreply.github.com>
2024-02-08 06:42:36 +0000

    conditional checkboxes working

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -3,18 +3,11 @@ import Checkbox from './Checkbox';
 import Dropdown from './Dropdown';
 
 const Page2 = ({ onPrev, onNext, data, setData }) => {
-  const [checkboxes, setCheckboxes] = useState({
-    checkbox1: false,
-    checkbox2: false,
-    checkbox3: false,
-  });
-
-  const handleCheckboxChange = (e) => {
-    setCheckboxes({ ...checkboxes, [e.target.name]: e.target.checked });
-  };
+  const [selectedOption, setSelectedOption] = useState('');
 
   const handleDropdownChange = (e) => {
-    setData({ ...data, gameType: e.target.value });
+    const selectedValue = e.target.value;
+    setSelectedOption(selectedValue);
   };
 
   // Example options for the dropdown
@@ -27,30 +20,38 @@ const Page2 = ({ onPrev, onNext, data, setData }) => {
   return (
     <div>
       <h2>Page 2</h2>
-      <Checkbox
-        label="Checkbox 1"
-        name="checkbox1"
-        checked={checkboxes.checkbox1}
-        onChange={handleCheckboxChange}
-      />
-      <Checkbox
-        label="Checkbox 2"
-        name="checkbox2"
-        checked={checkboxes.checkbox2}
-        onChange={handleCheckboxChange}
-      />
-      <Checkbox
-        label="Checkbox 3"
-        name="checkbox3"
-        checked={checkboxes.checkbox3}
-        onChange={handleCheckboxChange}
-      />
       <Dropdown
         label="Dropdown"
         options={dropdownOptions}
-        value={data.gameType}
+        value={selectedOption}
         onChange={handleDropdownChange}
       />
+      <br />
+      {/* Conditional rendering of checkboxes based on dropdown selection */}
+      {(selectedOption === 'DeathMatch' || selectedOption === 'Elimination') && (
+        <Checkbox
+          label="Checkbox 1"
+          name="checkbox1"
+          checked={data.checkbox1}
+          onChange={(isChecked) => setData({ ...data, checkbox1: isChecked })}
+        />
+      )}
+      {selectedOption === 'Elimination' && (
+        <Checkbox
+          label="Checkbox 2"
+          name="checkbox2"
+          checked={data.checkbox2}
+          onChange={(isChecked) => setData({ ...data, checkbox2: isChecked })}
+        />
+      )}
+      {selectedOption === 'Battle Royal' && (
+        <Checkbox
+          label="Checkbox 3"
+          name="checkbox3"
+          checked={data.checkbox3}
+          onChange={(isChecked) => setData({ ...data, checkbox3: isChecked })}
+        />
+      )}
       <br />
       <button onClick={onPrev}>Previous</button>
       <button onClick={onNext}>Next</button>
